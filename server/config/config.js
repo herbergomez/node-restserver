ddc7c7a15b2d23aff9b6fe8e0026dea1df392fbc
Herber Gomez <herberoswaldo.gomez@gmail.com>
2021-02-01 22:06:57 -0600

    Variable de entorno para Base de datos Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://devaky10:BuNl23uGfqr5YUCu@cluster0.5cuac.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
